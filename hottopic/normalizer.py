6f9af83b1a695f76e7b01a531d7137e3e4a41955
NickCrews <nicholas.b.crews@gmail.com>
2018-04-07 18:05:01 -0600

    norming actually works

--- hottopic/normalizer.py
+++ hottopic/normalizer.py
@@ -10,6 +10,7 @@ class Normalizer(object):
 
     def __init__(self, fits=None):
         self.fits = fits
+        self.num_channels = None if self.fits is None else len(self.fits)+1
 
     def fit(self, inputs):
         '''Find the mean and std dev of each channel for this group of inputs
@@ -17,14 +18,16 @@ class Normalizer(object):
         self.num_channels = inputs[0].shape[-1]
         self.fits = []
         for i in range(1, self.num_channels):
-            channel = [inp[:,:,i] for inp in inputs]
-            s = sum(np.nansum(c) for c in channel)
-            N = sum(c.size for c in channel)
-            mean =  s/N
-            devs_squared = [(c-mean)**2 for c in channel]
-            sum_devs_squared = sum(np.nansum(d) for d in devs_squared)
-            std = np.sqrt(sum_devs_squared/N)
-            print(mean, std)
+            print("normalizing channel", i)
+            channel = np.concatenate([inp[:,:,i].ravel() for inp in inputs])
+            # s = sum(np.nansum(c) for c in channel)
+            # N = sum(c.size for c in channel)
+            # mean =  s/N
+            # devs_squared = [(c-mean)**2 for c in channel]
+            # sum_devs_squared = sum(np.nansum(d) for d in devs_squared)
+            # std = np.sqrt(sum_devs_squared/N)
+            mean, std = np.nanmean(channel), np.nanstd(channel)
+            # print(mean, std)
             self.fits.append((mean, std))
 
     def normalize(self, inputs):
@@ -35,15 +38,17 @@ class Normalizer(object):
         else:
             return [self._normalize(inp) for inp in inputs]
 
-    def _normalize(self, input):
-        if input.shape[-1] != self.num_channels:
+    def _normalize(self, inp):
+        if inp.shape[-1] != self.num_channels:
             raise ValueError("Fitted with inputs that have a different number of channels")
 
+        inp = inp.copy()
         for i, (mean, std) in enumerate(self.fits, start=1):
             # ignore the first channel, starting_perim
             channel = inp[:,:,i]
             channel = (channel-mean) / std
-        return input
+            inp[:,:,i] = channel
+        return inp
 
     def save(self, fname):
         if self.fits is None:
@@ -63,7 +68,7 @@ class Normalizer(object):
             fname += '.json'
         with open(fname, 'r') as fp:
             fits = json.load(fp)
-        return PreProcessor(fits=fits)
+        return Normalizer(fits=fits)
 
     def __repr__(self):
         return 'Normalizer({})'.format(self.fits)
